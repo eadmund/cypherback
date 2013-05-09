d6ea5d4e197c65c26db12f6d45e293115cd1c773
Robert Uhl <ruhl@octopodial-chrome.com>
2013-05-09 07:54:52 -0600

    GPLed entire codebase

--- src/cypherback/commands/cypherback.go
+++ src/cypherback/commands/cypherback.go
@@ -1,3 +1,20 @@
+// Copyright 2013 Robert A. Uhl.  All rights reserved.
+//
+// This file is part of cypherback.
+//
+// Cypherback is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Cypherback is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Cypherback.  If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
